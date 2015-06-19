40f33b05d2c29b4a46aeb1699518654a7b55cda4
Jay-Plumb <jaymjplumb@gmail.com>
2015-06-19 14:30:48 -0700

    added several setters

--- backboneTutorial/todoApp/model.js
+++ backboneTutorial/todoApp/model.js
@@ -24,8 +24,29 @@ var todo2 = new Todo({
 	completed: true
 });
 
+// Getters
 console.log(todo2.get('title'));
 console.log(todo2.get('completed'));
 
+// Setters
+var todo3 = new Todo({
+	title: "set through instantiation"
+});
 
+console.log('Todo title: ' + todo3.get('title'));
+console.log('Completed: ' + todo3.get('completed'));
 
+// Set single attribute value at a time through Model.set();
+todo3.set('title', "Title attribute set through Model.set()");
+console.log('Todo title: ' + todo3.get('title'));
+console.log('Completed: ' + todo3.get('completed'));
+
+// Set a map of attributes through Model.set()
+todo3.set({
+	title: "Both attributes set through Model.set()",
+	completed: true
+});
+
+// Both attributes set through Model.set()
+console.log('Todo title: ' + todo3.get('title'));
+console.log('Completed: ' + todo3.get('completed'));
