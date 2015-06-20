6d3e233e874ad8f25394a70fb599cbe7f49c6d51
Jay-Plumb <jaymjplumb@gmail.com>
2015-06-20 14:00:06 -0700

    removed backbone practive

--- backboneTutorial/backbonePractice/view.js
+++
@@ -1,9 +0,0 @@
-// Ecample view that creates a DOM element but is not appended to the DOM 
-var TodosView = Backbone.View.extend({
-	tagName: 'ul', // required but defaults to 'div' if not set
-	className: 'container', // Optional. You can assign multiple classes to this property: e.g. 'container homepage'
-	id: 'todos', // Optional
-});
-
-var todosView = new TodosView();
-console.log(todosView.el);
