8038e4a6641c0026000949dd1d974c046c663a75
Jay-Plumb <jaymjplumb@gmail.com>
2015-06-20 23:41:25 -0700

    detecting and silencing changed events

---
+++ backboneTutorial/backboneBasics/models/person.js
@@ -0,0 +1,12 @@
+var Person = new Backbone.Model();
+
+Person.on("change:firstName", function(){
+	console.log('Name changed');
+});
+
+Person.set({firstName: 'Andrew'});
+Person.set({firstName: 'Jeremy'}, {silent:true});
+
+console.log(Person.hasChanged("firstName"));
+
+console.log(Person.hasChanged(null));
