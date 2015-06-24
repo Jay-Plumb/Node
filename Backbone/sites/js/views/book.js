20ab76090e8e29ff0569ec0eca14c5715e4905d1
Jay-Plumb <jaymjplumb@gmail.com>
2015-06-23 17:04:24 -0700

    adding delete functionality to view

--- Backbone/sites/js/views/book.js
+++ Backbone/sites/js/views/book.js
@@ -1,9 +1,22 @@
 var app = app || {};
 
 app.BookView = Backbone.View.extend({
+
 	tagName: 'div',
 	className: 'bookContainer',
 	template: _.template( $('#bookTemplate').html() ),
+	// Wire the delete button
+	events: {
+		'click .delete': 'deleteBook'
+	},
+
+	deleteBook: function() {
+		// Delete model
+		this.model.destroy();
+
+		// Delete view
+		this.remove();
+	},
 
 	render: function() {
 		// this.el is what we defined in tagName. Use $el to get access to jQuery html() function
